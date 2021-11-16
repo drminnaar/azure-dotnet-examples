c32b475321881ee1a73d92b6d009207b00d3d7a3
drminnaar <douglas.minnaar@gmail.com>
2021-11-17 09:46:25 +1300

    Add storage blob 'getting started' example

---
+++ storage/storage-container/GettingStarted/README.md
@@ -0,0 +1,53 @@
+---
+page_type: sample
+languages:
+- C#
+products:
+- Azure
+- Azure Storage
+- Aszure Storage Blobs
+description: "Demonstrates how to use Azure.Storage.Blobs SDK"
+---
+
+# Azure Storage Queue - Getting Started
+
+A simple example demonstrating how to use Azure.Storage.Blobs SDK.
+
+The following concepts are demonstrated:
+
+- Create container
+- Upload file to container
+- Retrieve file from container
+- Download a file from container
+- Delete file from container
+- Delete container
+
+---
+
+## Getting Started
+
+Please see [Azure Storage Queue - Getting Started] guide where I explain how to get started with Azure storage using both an emulator (Azurite) and the actual Azure Storage Service.
+
+## Configure Connection String
+
+```powershell
+# configure connection string to use local emulator
+
+dotnet user-secrets set "ConnectionStrings:StorageQueue" "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
+```
+
+## Run App
+
+```powershell
+# change to project folder
+cd .\StorageQueue.GettingStarted\
+
+# run project
+dotnet run
+```
+
+![storage-blob-1](https://user-images.githubusercontent.com/33935506/142062351-37d21cfb-c355-4ba4-a3b9-cc83a5db24af.png)
+
+---
+
+[Azure Storage Queue - Getting Started]: https://github.com/drminnaar/azure-dotnet-examples/blob/main/storage/README.md
---
+++ storage/storage-container/GettingStarted/StorageContainer.GettingStarted/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using FakeData;
+using FakeData.Reviews;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console;
+
+namespace StorageContainer.GettingStarted
+{
+    internal sealed class Program
+    {
+        internal static async Task Main()
+        {
+            var provider = ConfigureServices();
+            var serviceClient = provider.GetRequiredService<BlobServiceClient>();
+
+            DisplayHeader();
+
+            // create file
+            Display("CREATE FILE");
+            var file = await CreateFile(provider);
+
+            // create container
+            Display("CREATE CONTAINER");
+            var containerName = $"getting-started-{Guid.NewGuid()}";
+            var containerClient = await serviceClient.CreateBlobContainerAsync(containerName);
+
+            // upload file
+            Display("UPLOAD FILE");
+            var blobClient = containerClient.Value.GetBlobClient(file.FileName);
+            await blobClient.UploadAsync(path: file.Path, overwrite: true);
+
+            // get file
+            Display("GET FILE CONTENT");
+            blobClient = containerClient.Value.GetBlobClient(file.FileName);
+            using var stream = new StreamReader(await blobClient.OpenReadAsync());
+            var fileContentFromBlob = await stream.ReadToEndAsync();
+            Console.WriteLine($"\n{fileContentFromBlob}");
+
+            // download file
+            Display("DOWNLOAD FILE");
+            blobClient = containerClient.Value.GetBlobClient(file.FileName);
+            await blobClient.DownloadToAsync(file.DownloadPath);
+
+            // delete file
+            Display("DELETE FILE");
+            blobClient = containerClient.Value.GetBlobClient(file.FileName);
+            await blobClient.DeleteIfExistsAsync(Azure.Storage.Blobs.Models.DeleteSnapshotsOption.None);
+
+            // delete container
+            Display("DELETE CONTAINER");
+            await containerClient.Value.DeleteIfExistsAsync();
+
+            // delete data folder
+            Directory.Delete(file.Directory, true);
+
+            Console.WriteLine();
+            AnsiConsole.Write(new Text("\nDEMO COMPLETE", new Style(foreground: Color.Yellow2)).Centered());
+        }
+
+        private static void DisplayHeader()
+        {
+            AnsiConsole.Clear();
+            AnsiConsole.Write(new Text("AZURE STORAGE BLOB", new Style(foreground: Color.Yellow2)).Centered());
+            AnsiConsole.Write(new Text("GETTING STARTED", new Style(foreground: Color.Yellow2)).Centered());
+            Console.WriteLine();
+        }
+
+        private static async Task<dynamic> CreateFile(IServiceProvider provider)
+        {
+            var faker = provider.GetRequiredService<FakeEntityGeneratorBase<Review>>();
+
+            var dataDirectory = $"./data-{Guid.NewGuid()}";
+
+            if (!Directory.Exists(dataDirectory))
+                Directory.CreateDirectory(dataDirectory);
+
+            var (fileName, fileContent) = faker.GenerateFakes(10).ToRandomFile();
+            var localFilePath = Path.Combine(dataDirectory, fileName);
+            await File.WriteAllTextAsync(localFilePath, fileContent);
+
+            return new
+            {
+                Directory = dataDirectory,
+                FileContent = fileContent,
+                FileName = fileName,
+                Path = localFilePath,
+                DownloadPath = localFilePath.Replace(".json", "DOWNLOADED.json")
+            };
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>(optional: false, reloadOnChange: true)
+                .Build();
+
+            return new ServiceCollection()
+                .AddSingleton(new BlobServiceClient(configuration.GetConnectionString("StorageAccount")))
+                .AddSingleton<FakeEntityGeneratorBase<Review>>(new FakeReviewGenerator())
+                .BuildServiceProvider();
+        }
+
+        private static void Display(string text)
+        {
+            AnsiConsole.MarkupLine($"\n[bold yellow2]{Emoji.Known.RightArrow} {text} ...[/]");
+            Console.WriteLine();
+        }
+    }
+}
